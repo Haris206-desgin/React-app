04bb1889dde6a42f5d8382691f2d26b6b3a94f9d
Haris206-desgin <harissk504@gmail.com>
2026-01-01 00:05:15 +0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,28 +1,6 @@
 
 
 
-// import Hero from "./components/Hero/Hero";
-
-// function App() {
-//   return <Hero />;
-// }
-
-// export default App;
-
-// import Hero from "./components/Hero/Hero";
-// import Features from "./components/Features/Features";
-
-// function App() {
-//   return (
-//     <>
-//       <Hero />
-//       <Features />
-//     </>
-//   );
-// }
-
-// export default App;
-
 
 
 
@@ -30,7 +8,7 @@
 import Hero from "./components/Hero/Hero";
 import Features from "./components/Features/Features";
 import Connection from "./components/connection/connection";
-import Install from "./components/install/install.jsx";
+import Install from "./components/Install/Install"
 import CTA from "./components/CTA/CTA.jsx";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Download from "./components/Download/Download";
