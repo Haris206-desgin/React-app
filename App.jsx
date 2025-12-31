1d82b5347ce9940e2259dc47ac821ae26151e241
Haris206-desgin <harissk504@gmail.com>
2025-12-31 23:16:16 +0500

    intial commit

---
+++ App.jsx
@@ -0,0 +1,55 @@
+
+
+
+// import Hero from "./components/Hero/Hero";
+
+// function App() {
+//   return <Hero />;
+// }
+
+// export default App;
+
+// import Hero from "./components/Hero/Hero";
+// import Features from "./components/Features/Features";
+
+// function App() {
+//   return (
+//     <>
+//       <Hero />
+//       <Features />
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+import Hero from "./components/Hero/Hero";
+import Features from "./components/Features/Features";
+import Connection from "./components/connection/connection";
+// import Install from "./components/Install/install";
+import CTA from "./components/CTA/CTA.jsx";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Download from "./components/Download/Download";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Connection />
+      <Install />
+      <CTA />
+      <Testimonials />
+      <Download />
+    </>
+  );
+}
+
+export default App;
+
+
+
